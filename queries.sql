680141158adb5ff6be06ba76b4112133f06a057c
WillPaixao <96320885+WillPaixao@users.noreply.github.com>
2024-11-15 20:03:39 -0300

    Pretty-printing is my passion

--- queries.sql
+++ queries.sql
@@ -12,11 +12,11 @@ WITH
 			   GROUP BY V.ID) AS UDA JOIN
 					Video AS V ON UDA.ID = V.ID JOIN
 						Video_Snapshot AS VS ON V.ID = VS.Video_ID AND
-												UDA.Ultima_data = VS.Data)
+																		UDA.Ultima_data = VS.Data)
 SELECT DISTINCT CS.Titulo AS Canal, VR.Total_de_visualizacoes
 FROM (SELECT Canal_ID, SUM(Numero_de_visualizacoes) AS Total_de_visualizacoes
 	  FROM Video_mais_recente
-      GROUP BY Canal_ID) AS VR JOIN
+    	GROUP BY Canal_ID) AS VR JOIN
 		Canal_Snapshot AS CS USING (Canal_ID)
 ORDER BY VR.Total_de_visualizacoes DESC;
 
@@ -29,11 +29,11 @@ canal no Em Alta de um país em uma data.
 SELECT CS.Titulo AS Canal, COUNT(CS.Canal_ID) AS Numero_de_vezes_no_Trend
 FROM Trend AS T JOIN
 		Video_Snapshot AS VS ON T.Video_ID = VS.Video_ID AND
-								T.Video_Snapshot_Data = VS.Data JOIN
+														T.Video_Snapshot_Data = VS.Data JOIN
 			Video AS V ON VS.Video_ID = V.ID JOIN
 				Canal AS C ON V.Canal_ID = C.ID JOIN
 					Canal_Snapshot AS CS ON C.ID = CS.Canal_ID AND
-											VS.Data = CS.Data
+																	VS.Data = CS.Data
 GROUP BY CS.Titulo
 ORDER BY Numero_de_vezes_no_Trend DESC;
 
@@ -48,7 +48,7 @@ FROM (SELECT Video_ID, MAX(Video_Snapshot_Data) AS Ultima_data
 	  FROM Tag
       GROUP BY Video_ID) AS VMR JOIN
 		Tag AS T ON VMR.Video_ID = T.Video_ID AND
-					VMR.Ultima_data = T.Video_Snapshot_Data
+															 VMR.Ultima_data = T.Video_Snapshot_Data
 GROUP BY Tag
 ORDER BY Numero_de_usos DESC;
 
@@ -63,7 +63,7 @@ FROM (SELECT Canal_ID, MAX(Canal_Snapshot_Data) AS Ultima_data
 	  FROM Palavra_chave
       GROUP BY Canal_ID) AS CMR JOIN
 		Palavra_chave AS PC ON CMR.Canal_ID = PC.Canal_ID AND
-							   CMR.Ultima_data = PC.Canal_Snapshot_Data
+							   					 CMR.Ultima_data = PC.Canal_Snapshot_Data
 GROUP BY Palavra_chave
 ORDER BY Numero_de_usos DESC;
 
@@ -81,7 +81,7 @@ WITH
 					Canal AS C ON CS.Canal_ID = C.ID JOIN
 						Video AS V ON C.ID = V.Canal_ID JOIN
 							Video_Snapshot AS VS ON V.ID = VS.Video_ID AND
-													CS.Data = VS.Data)
+																			CS.Data = VS.Data)
 (SELECT Codigo AS Pais_sede, COUNT(Codigo) AS Numero_de_aparicoes
  FROM Paises_em_alta
  WHERE Codigo IS NOT NULL
